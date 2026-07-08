d9d00f0d91a02b5f7bdc5d49e9dd3300520800c2
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-07-08 11:37:29 +0600

    JAVASCRIPT

---
+++ 08numberMe.js
@@ -0,0 +1,17 @@
+var num = 20;
+num = toString(num);
+console.log(typeof (num));
+var value = "20";
+value = parseInt(value);
+console.log(typeof (value));
+var por = 2.5641;
+// show from first to ...
+console.log(por.toPrecision(2));
+// show after . 
+console.log(por.toFixed(2));
+// it convert string to number
+console.log(Number("12"));
+// show datatype
+console.log(typeof(Number("12")));
+
+
