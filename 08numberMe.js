ffb218ef87c4fed128962ef1a59f05f249c3c028
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-08-05 12:58:29 +0600

    add new code

--- 08numberMe.js
+++ 08numberMe.js
@@ -1,17 +1,19 @@
 var num = 20;
-num = toString(num);
+num = toString(num);//convert in string
 console.log(typeof (num));
 var value = "20";
-value = parseInt(value);
+value = parseInt(value);//string in int
 console.log(typeof (value));
 var por = 2.5641;
-// show from first to ...
+// show from first to given number ...
 console.log(por.toPrecision(2));
-// show after . 
+// show after . 2 values
 console.log(por.toFixed(2));
 // it convert string to number
 console.log(Number("12"));
 // show datatype
 console.log(typeof(Number("12")));
+//convert bool to number
+console.log(Number(false));
 
 
---
+++ 14area.js
@@ -0,0 +1,4 @@
+var base = parseFloat(prompt("Enter base = "));
+var height = parseFloat(prompt("Enter height = "));
+var area = base * height;
+document.write("Area="+area);
