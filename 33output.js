a127be097a5a2fb1b53d66bcd00833461a6a14d7
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-08-30 23:01:20 +0600

    add funcation related codefiles

---
+++ 33output.js
@@ -0,0 +1,11 @@
+// (immediarely invokeable funcation expressions)
+(function display(message){
+console.log( message)
+
+})("faysal");
+
+// function expression 
+var display2 = function dispalymessage(){
+    console.log ("My name is Faysal Ahmed Ramim");
+};
+display2();
