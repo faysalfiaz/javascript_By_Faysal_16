d613e8a2853f006cc3b39ca34d68db0e1584b48a
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-08-31 23:53:44 +0600

    Date function & obs

---
+++ 43output.js
@@ -0,0 +1,17 @@
+var date  = new Date(); 
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var currentDate = date.getDate();
+console.log(currentDate);
+
+var currentday = date.getDay();
+console.log(currentday);
+
+var currentHour = date.getHours();
+console.log(currentHour);
